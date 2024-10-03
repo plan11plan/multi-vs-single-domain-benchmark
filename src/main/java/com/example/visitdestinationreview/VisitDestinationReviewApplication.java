2662f66bb6b6e8e9a0271c8c4fb578edb27aeadc
plan11plan <tjtfy7028@gmail.com>
2024-10-03 16:18:53 +0900

    first commmit

---
+++ src/main/java/com/example/visitdestinationreview/VisitDestinationReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.visitdestinationreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitDestinationReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitDestinationReviewApplication.class, args);
+    }
+
+}
